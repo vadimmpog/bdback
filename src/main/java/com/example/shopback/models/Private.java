093f853aeebe166c70468e9a5ff19051e271b694
Vadim <vadimmm120@yandex.ru>
2021-12-05 00:08:20 +0300

    Private update

--- src/main/java/com/example/shopback/models/Private.java
+++ src/main/java/com/example/shopback/models/Private.java
@@ -26,4 +26,15 @@ public class Private {
     private String insurance;
     private String address;
     private Integer employerID;
+
+    public void update(Private p){
+        if (this.firstname == null || this.firstname.equals("")) this.setFirstname(p.getFirstname());
+        if (this.lastname == null || this.lastname.equals("")) this.setLastname(p.getLastname());
+        if (this.birthdate == null) this.setBirthdate(p.getBirthdate());
+        if (this.phonenum == null || this.phonenum.equals("")) this.setPhonenum(p.getPhonenum());
+        if (this.insurance == null || this.insurance.equals("")) this.setInsurance(p.getInsurance());
+        if (this.address == null || this.address.equals("")) this.setAddress(p.getAddress());
+        if (this.employerID == null || this.employerID == 0) this.setEmployerID(p.getEmployerID());
+
+    }
 }
